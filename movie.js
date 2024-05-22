04c80031cae70382c1be101818fbb4f300ecaea0
gaetano-94 <gaetano.panico1994@gmail.com>
2024-05-22 14:58:54 +0200

    feat: array di oggetti per film e serie tv

---
+++ movie.js
@@ -0,0 +1,33 @@
+// Array di oggetti rappresentanti film e serie tv
+const media = [
+  {
+    title: 'Jaws',
+    year: 1975,
+    genre: 'Drama',
+    rating: 8,
+    type: 'movie',
+  },
+  {
+    title: 'Breaking Bad',
+    year: 2008,
+    genre: 'Drama',
+    rating: 9.5,
+    type: 'tv',
+    seasons: 5,
+  },
+  {
+    title: 'Inception',
+    year: 2010,
+    genre: 'Sci-Fi',
+    rating: 8.8,
+    type: 'movie',
+  },
+  {
+    title: 'Stranger Things',
+    year: 2016,
+    genre: 'Horror',
+    rating: 8.7,
+    type: 'tv',
+    seasons: 4,
+  },
+];
